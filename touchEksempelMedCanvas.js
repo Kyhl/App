eb41fbf631b98839967cc62189907512d138c00b
Kyhl <kasper.sebastian@gmail.com>
2017-01-10 20:16:49 +0100

    It now works

--- touchEksempelMedCanvas.js
+++ touchEksempelMedCanvas.js
@@ -5,21 +5,28 @@ var ygl;
 
 
 function opdaterCanvas(){
+	var c = document.getElementById("mycanvas");
+	var ctx = c.getContext("2d");
 	var x=event.touches[0].pageX;
 		var y = event.touches[0].pageY;
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
+	//ctx.beginPath();
+	//ctx.moveTo(x,y);
+	ctx.lineTo(x,y);
 	ctx.stroke();
+	//ctx.closePath();
 }
 function init(){
 	var touchzone = document.getElementById("mycanvas");
-	touchzone.addEventListener("touchmove",opdaterCanvas,false)
-	touchzone.addEventListener("touchend",end,false)
+	touchzone.addEventListener("touchstart",opdaterCanvas,false);
+	touchzone.addEventListener("touchmove",opdaterCanvas,false);
+	touchzone.addEventListener("touchend",end,false);
 	ctx=touchzone.getContext("2d");
 	mycanvas.width=mycanvas.width;
-	ctx.fillStyle="yellow";
+	ctx.fillStyle="red";
 	ctx.fillRect(0,0,mycanvas.width,mycanvas.height);
 }
+
+
 
 function end(e){
 	e.preventDefault();
